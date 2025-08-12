791bbb069d9b7526260a53ab28811c6684f6ef9a
Julio Cesar <juliocesargcosta123@gmail.com>
2025-08-12 03:06:37 +0100

    feat/update_images_dynamically
    - added a new functionality in where is possible to adjust the parameters of an image in real-time

--- test_image.py
+++ test_image.py
@@ -4,8 +4,11 @@ from Source.LaneDetector import LaneDetector
 if __name__ == "__main__":
     filepath = r"Resources/images/lane.jpg"
     image = cv2.imread(filepath)
-    LD = LaneDetector(pipeline="pipeline.json", controls=False)
-    output_image = LD.frame_processor(image, alpha=1.0, beta=0.5)
-    cv2.imshow("Lane Detector", output_image)
-    cv2.waitKey(0)
+    LD = LaneDetector(pipeline="pipeline.json", controls=True)
+    while True:
+        output_image = LD.frame_processor(image, alpha=1.0, beta=0.5)
+        cv2.imshow("Lane Detector", output_image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.imwrite(f"{filepath.split('.')[0]}_output.json", output_image)
+            break
     cv2.destroyAllWindows()
